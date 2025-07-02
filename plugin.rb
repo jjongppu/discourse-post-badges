3aa126461e216d741029e6585ab83f56169e15ac
jonggil.kim <jonggil.kim@dkbmc.com>
2025-07-02 13:16:31 +0900

    출력 기준 변경

--- plugin.rb
+++ plugin.rb
@@ -3,7 +3,7 @@
 # version: 0.1
 # authors: 쫑뿌
 
-after_initialize do
+  after_initialize do
     %w[대표 배지1 대표 배지2 대표 배지3].each_with_index do |field, idx|
       add_to_serializer(:post, "representative_badge_#{idx + 1}".to_sym) do
         object.user&.custom_fields[field]
@@ -11,3 +11,11 @@ after_initialize do
     end
   end
   
+  add_to_serializer(:user_card, :gamification_level_info) do
+    begin
+      DiscourseGamification::LevelHelper.progress_for(object.id)
+    rescue => e
+      Rails.logger.error("Gamification Level Error: #{e.message}")
+      nil
+    end
+  end
