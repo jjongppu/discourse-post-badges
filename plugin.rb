b9abbfae17025863104e942049a9da17a67acb0f
jonggil.kim <jonggil.kim@dkbmc.com>
2025-07-02 12:47:19 +0900

    출력 기준 변경

---
+++ plugin.rb
@@ -0,0 +1,13 @@
+# name: discourse-representative-badge
+# about: 게시글에 대표 배지1~3 표시용 커스텀 필드 전달
+# version: 0.1
+# authors: 쫑뿌
+
+after_initialize do
+    %w[대표 배지1 대표 배지2 대표 배지3].each_with_index do |field, idx|
+      add_to_serializer(:post, "representative_badge_#{idx + 1}".to_sym) do
+        object.user&.custom_fields[field]
+      end
+    end
+  end
+  
