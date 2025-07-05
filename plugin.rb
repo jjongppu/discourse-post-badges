4dc1906d2d9b2c77b92865a0bbf2cd68985e221d
jjongppu <jjongppu@naver.com>
2025-07-05 17:18:44 +0900

    feat: show selected badges

--- plugin.rb
+++ plugin.rb
@@ -3,10 +3,10 @@
 # version: 0.1
 # authors: 쫑뿌
 
-  after_initialize do
-    %w[대표 배지1 대표 배지2 대표 배지3].each_with_index do |field, idx|
-      add_to_serializer(:post, "representative_badge_#{idx + 1}".to_sym) do
-        object.user&.custom_fields[field]
-      end
+after_initialize do
+  %w[대표 배지1 대표 배지2].each_with_index do |field, idx|
+    add_to_serializer(:post, "representative_badge_#{idx + 1}".to_sym) do
+      object.user&.custom_fields[field]
     end
   end
+end
